5c4b1fad10f6625022b44ba1e4ccc30361e14d60
plocate <pierre-antoine.locatelli@u-psud.fr>
2018-01-25 10:35:48 +0100

    Ajout d'une premier base avec une donnée de chaque, à compléter

--- dark/base1.lua
+++ dark/base1.lua
@@ -1,22 +1,75 @@
 local base = {}
-base["anime"]  = {}
+base["anime"]  = {
+	{
+		["title"] = "Code Geass: Hangyaku no Lelouch",
+		["studios"] = {"Sunrise"},
+		["episodes"] = 25,
+		["status"] = "finished",
+		["genres"] = {"action", "mecha", "military", "school", "sci-fi", "super power"},
+		["synopsis"] = "In the year 2010, the Holy Empire of Britannia is establishing itself as a dominant military nation, starting with the conquest of Japan. Renamed to Area 11 after its swift defeat, Japan has seen significant resistance against these tyrants in an attempt to regain independence. Lelouch Lamperouge, exiled prince of Britannia, unfortunately finds himself caught in a crossfire between the two nations' armed forces. He is able to escape, however, thanks to the timely appearance of a mysterious girl named C.C., who bestows upon him Geass, the 'Power of Kings.' Realizing the vast potential of his newfound 'power of absolute obedience,' Lelouch embarks upon a perilous journey as the masked vigilante known as Zero, leading a merciless onslaught against Britannia in order to get revenge once and for all.",
+		["popularity"] = 7,
+		["relatedWorks"]["adaptation"] = {"Code Geass: Hangyaku no Lelouch"},
+		["relatedWorks"]["anime"] = {"Code Geass: Hangyaku no Lelouch Picture Drama", "Code Geass: Hangyaku no Lelouch - Nunnally in Wonderland"},
+		}
+		["characters"]["main"] = {"Lelouch Lamperouge", "C.C.", "Kallen Stadtfeld", "Suzaku Kururugi"},
+		["characters"]["supporting"] = {"Jeremiah Gottwald", "Shirley Fenette", "Lloyd Asplund", "Euphemia li Britannia", "Nunnally Lamperouge"},
+		["staff"] = {"Yutaka Maseba", "Gorou Taniguchi", "Yasuo Urakami", "Makoto Baba"},
+		["review"] = {
+			{["score"]=9, ["helpful"]=1975, ["text"]="I was incredibly skeptical about Code Geass at first, but I'm very pleased to say that I was greatly surprised."},	
+			{["score"]=10, ["helpful"]=829, ["text"]="This is an exciting and epic anime and it's over the top."}
+		}
+	}
+
+}
 
 base["manga"] = {
 	{
-	["title"] = "Bleach",
-	["volumes"] = 74,
-	["chapters"] = 705,
-	["status"] = "finished",
-	["genres"] = {"action", "adventure", "shounen", "super power", "supernatural"},
-	["synopsis"] = "Ichigo Kurosaki has always been able to see ghosts, but this ability doesn't change his life nearly as much as his close encounter with Rukia Kuchiki, a Soul Reaper and member of the mysterious Soul Society. While fighting a Hollow, an evil spirit that preys on humans who display psychic energy, Rukia attempts to lend Ichigo some of her powers so that he can save his family; but much to her surprise, Ichigo absorbs every last drop of her energy. Now a full-fledged Soul Reaper himself, Ichigo quickly learns that the world he inhabits is one full of dangerous spirits and, along with Rukia—who is slowly regaining her powers—it's Ichigo's job to protect the innocent from Hollows and help the spirits themselves find peace.",
-	["relatedManga"] = {"Bleach", "Bleach: Letters From the Other Side", "	Bleach: The Unforgivens"},
-	["Characters"]["Main"] = {"Kurosaki Ichigo", "Kuchiki Rukia", "Inoue Orihime", "Ishida Uryuu", "Sado Yasutora"},
-	["Characters"]["Supporting"] = {"Zaraki Kenpachi", "Histugaya Toushirou", "Cifer Ulquiorra", "Urahara Kisuke", "Kuchiji Byakuya"},
-	["review"] = {
-		{["score"]=5, ["helpful"]=245, ["text"]="Very good, it talks about death and samurai just like Naruto"},	
-		{["score"]=8, ["helpful"]=21, ["text"]="Marry me Ulquiorra"},	
-	}
+		["title"] = "Bleach",
+		["authors"] = {"Tite Kubo"},
+		["volumes"] = 74,
+		["chapters"] = 705,
+		["status"] = "finished",
+		["genres"] = {"action", "adventure", "shounen", "super power", "supernatural"},
+		["synopsis"] = "Ichigo Kurosaki has always been able to see ghosts, but this ability doesn't change his life nearly as much as his close encounter with Rukia Kuchiki, a Soul Reaper and member of the mysterious Soul Society. While fighting a Hollow, an evil spirit that preys on humans who display psychic energy, Rukia attempts to lend Ichigo some of her powers so that he can save his family; but much to her surprise, Ichigo absorbs every last drop of her energy. Now a full-fledged Soul Reaper himself, Ichigo quickly learns that the world he inhabits is one full of dangerous spirits and, along with Rukia—who is slowly regaining her powers—it's Ichigo's job to protect the innocent from Hollows and help the spirits themselves find peace.",
+		["popularity"] = 3,
+		["relatedWorks"]["manga"] = {"Bleach", "Bleach: Letters From the Other Side", "	Bleach: The Unforgivens"},
+		["relatedWorks"]["adaptation"] = {"Bleach"},
+		["characters"]["main"] = {"Kurosaki Ichigo", "Kuchiki Rukia", "Inoue Orihime", "Ishida Uryuu", "Sado Yasutora"},
+		["characters"]["supporting"] = {"Zaraki Kenpachi", "Histugaya Toushirou", "Cifer Ulquiorra", "Urahara Kisuke", "Kuchiji Byakuya"},
+		["review"] = {
+			{["score"]=5, ["helpful"]=245, ["text"]="Very good, it talks about death and samurais just like Naruto"},	
+			{["score"]=8, ["helpful"]=21, ["text"]="Marry me Ulquiorra"}
+		}
 	}
 }
+
+base["character"] = {
+	{
+		["name"] = "Victorique de Blois",
+		["relatedManga"] = {"Gosick", "Gosick Recap"},
+		["relatedAnime"] = {"Gosick", "GosickS"},
+		["description"] = "Birthday: December 25, 1909. Height: 140 cm. The heroine of the story. Victorique has the appearance of a small, almost doll-like, girl with long golden hair and emerald eyes and a voice that sounds exactly like that of an older woman. It is mentioned once that she is 15 years old. Another important part of Victorique's appearance is a pipe that she smokes when thinking about something. She spends her days at the conservatory at the top of the library, reading several difficult books, often in different languages, simultaneously. She points at one wall of the library and has told Kazuya that she has read almost all the books over there. Along with her sharp tongue, abusive bluntness and eccentric attitude, she possesses amazing detective skills. After Victorique saves him from a near-arrest, Kazuya finds himself being entangled in every new mystery. Because her mother was a mistress, and according to Victorique 'a dangerous person,' she was imprisoned in the depths of the de Blois mansion where she developed very un-childlike mannerisms. She was sent off to school under strict orders to never leave the campus (although Grevil sometimes obtains special permission to take her out). When she first met Kazuya she didn't like him and thought that he was annoying and stupid for climbing up all the stairs in the library. She even ignored him and made fun of him, but gradually warmed up to him and began to trust and cherish him. She enjoys 'reconstructing chaos' and often complains of the boredom that causes her headaches. Victorique also seems to love sweets, since she's always really happy when Kujo gets her anything sweet. In the end she is shown to have found Kujou's family during/after the war and waits with Kazuya Ruri at the place of the returning soldiers. Her hair color is also shown to have turned silver. When he comes they walk hand in hand towards a great lightning where its seen that she wears a dress which looks like a wedding dress.",
+		["voiceActors"] = {"Aoi Yuuki", "Apphia Yu"},
+	}
+}
+
+
+base["professionnal"] = {
+	{
+		["name"] = "Tite Kubo",
+		["voiceActingRole"] = {
+			{["anime"] = "Bleach: Memories in the Rain", ["character"] = "Kon"}
+		}
+		["animeStaffRole"] = {
+			{["anime"] = "Aoi Bungaku Series", ["role"] = "Original Character Design"},
+			{["anime"] = "Bleach", ["role"] = "Original Creator"}
+		}
+		["publishedManga"] = {
+			{["manga"] = "Bleach", ["role"] = "Story & Art"},
+			{["anime"] = "Bleach 4-koma: Komaburi", ["role"] = "Story"}
+		}
+	}
+}
+
 
 
